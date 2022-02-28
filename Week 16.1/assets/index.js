7493e384c35bd75b1640a1e2d0a48387ba36743a
LikaSobolenko <91249749+LikaSobolenko@users.noreply.github.com>
2022-02-28 14:23:17 +0700

    Add files via upload

---
+++ Week 16.1/assets/index.js
@@ -0,0 +1,20 @@
+let numbers = [];
+
+function sum() {
+    while (true) {
+        let value = prompt('Введите число', '');
+        if (value === "" || value === null || !isFinite(value))
+            break;
+        numbers.push(+value)
+    }
+    numbers.sort();
+    alert(numbers);
+
+    let sum = 0;
+    for (let number of numbers) {
+        sum += number;
+    }
+    return sum;
+}
+
+alert(sum());
